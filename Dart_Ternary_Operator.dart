f99fbc8cb0b529ef3b3cbf3df6555ad1e61ee5f5
Andrew256 <141121118+Andrew256CRO@users.noreply.github.com>
2024-03-29 11:12:26 +0200

    Add files via upload

---
+++ Dart_Ternary_Operator.dart
@@ -0,0 +1,12 @@
+void main(){
+
+  // Operatorul ternar de comparatie
+
+  bool operatorTernar=false;
+  operatorTernar==true ? print('Nice') : print('Valeu');
+
+  int age=17;
+  bool canIBuyBere= age>=18 ? true : false;
+  print(canIBuyBere);
+
+}
